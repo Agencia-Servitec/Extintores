ff9e3e683a7c57b15ba6b872b1f3e281fd3da4f4
angelgt <angel.gamonal@agenciaservitec.com>
2023-08-14 12:56:38 -0500

    added firebaserc

--- src/components/public/home/InitialComponent.js
+++ src/components/public/home/InitialComponent.js
@@ -1,144 +1,142 @@
-import React from "react";
-import styled from "styled-components";
-import { ExtintoresBackground } from "../../../images";
-import { mediaQuery } from "../../../styles/constants/mediaQuery";
-import { Button } from "../ui";
+import React from 'react';
+import styled from 'styled-components';
+import { ExtintoresBackground } from '../../../images';
+import { mediaQuery } from '../../../styles/constants/mediaQuery';
+import { Button } from '../ui';
 
 export const InitialComponent = ({ onClickVisibleFormContact }) => {
-  return (
-    <Container id="inicio" bgImage={ExtintoresBackground}>
-      <div className="content-items">
-        <div className="content">
-          <div className="heading">
-            <h2 className="description">
-              VENTA DE EXTINTORES, MANTENIMIENTO Y RECARGAS
-            </h2>
-            <h3>MANTENIMIENTO Y REPARACIÓN</h3>
-            <h3>¡Tenemos la solución!</h3>
-            <div
-              styles={{
-                width: "100%",
-                display: "flex",
-                justifyContent: "start",
-              }}
-              onClick={() => {
-                onClickVisibleFormContact(true);
-              }}
-            >
-              <Button text="CONTACTANOS" type="primary" />
-            </div>
-          </div>
-        </div>
-        {/*<div className="content-img">*/}
-        {/*  <div className="wrapper-form">*/}
-        {/*    <FormContact />*/}
-        {/*  </div>*/}
-        {/*</div>*/}
-      </div>
-    </Container>
-  );
+	return (
+		<Container id="inicio" bgImage={ExtintoresBackground}>
+			<div className="content-items">
+				<div className="content">
+					<div className="heading">
+						<h2 className="description">
+							VENTA DE EXTINTORES, MANTENIMIENTO Y RECARGAS
+						</h2>
+						<h3>MANTENIMIENTO Y REPARACIÓN</h3>
+						<h3>¡Tenemos la solución!</h3>
+						<div
+							styles={{
+								width: '100%',
+								display: 'flex',
+								justifyContent: 'start',
+							}}
+							onClick={() => {
+								onClickVisibleFormContact(true);
+							}}
+						>
+							<Button text="CONTACTANOS" type="primary" />
+						</div>
+					</div>
+				</div>
+				{/*<div className="content-img">*/}
+				{/*  <div className="wrapper-form">*/}
+				{/*    <FormContact />*/}
+				{/*  </div>*/}
+				{/*</div>*/}
+			</div>
+		</Container>
+	);
 };
 
 const Container = styled.section`
-  width: 100%;
-  height: auto;
-  background: url(${({ bgImage }) => bgImage}) no-repeat scroll 0 0 / 100% 100%
-    rgba(0, 0, 0, 0);
-  background-position: center center;
-  background-size: cover;
+	width: 100%;
+	height: auto;
+	background: url(${({ bgImage }) => bgImage}) no-repeat scroll 0 0 / 100% 100%
+		rgba(0, 0, 0, 0);
+	background-position: center center;
+	background-size: cover;
 
-  ${mediaQuery.minTablet} {
-    padding: 4rem 0;
-  }
+	${mediaQuery.minTablet} {
+		padding: 4rem 0;
+	}
 
-  .content-items {
-    display: grid;
-    grid-template-columns: 1fr;
-    padding: 1rem;
-    justify-items: center;
+	.content-items {
+		display: grid;
+		grid-template-columns: 1fr;
+		padding: 1rem;
+		justify-items: center;
 
-    ${mediaQuery.minTablet} {
-      grid-template-columns: 1fr;
-      padding: 6rem 4rem 4rem 4rem;
-    }
+		${mediaQuery.minTablet} {
+			grid-template-columns: 1fr;
+			padding: 6rem 4rem 4rem 4rem;
+		}
 
-    .content {
-      // align-self: center;
-      padding-left: 0;
-      ${mediaQuery.minTablet} {
-        grid-template-columns: 1fr;
-        //padding-left: 2rem;
-      }
+		.content {
+			// align-self: center;
+			padding-left: 0;
+			${mediaQuery.minTablet} {
+				grid-template-columns: 1fr;
+				//padding-left: 2rem;
+			}
 
-      .heading {
-        text-align: center;
-        ${mediaQuery.maxMobile} {
-          padding: 2em 1rem 2rem 1rem;
-        }
-        .title {
-          color: ${({ theme }) => theme.colors.font1};
-          font-weight: 800;
-          font-family: "Intro Demo", sans-serif;
-          font-size: 1.2rem;
-          padding-top: 1.2rem;
-          ${mediaQuery.minTablet} {
-            grid-template-columns: 1fr;
-            padding-bottom: 1.2rem;
-          }
-        }
+			.heading {
+				text-align: center;
+				${mediaQuery.maxMobile} {
+					padding: 2em 1rem 2rem 1rem;
+				}
+				.title {
+					color: ${({ theme }) => theme.colors.font1};
+					font-weight: 800;
+					font-family: 'Intro Demo', sans-serif;
+					font-size: 1.2rem;
+					padding-top: 1.2rem;
+					${mediaQuery.minTablet} {
+						grid-template-columns: 1fr;
+						padding-bottom: 1.2rem;
+					}
+				}
 
-        .description {
-          color: ${({ theme }) => theme.colors.font1};
-          max-width: 70rem;
-          font-weight: 500;
-          font-size: 4rem;
-          font-family: "Intro Demo", sans-serif;
-          line-height: 4.5rem;
-          ${mediaQuery.minTablet} {
-            font-size: 5.4rem;
-          }
-          ${mediaQuery.maxMobile} {
-            font-size: 2.7rem;
-            line-height: 3.5rem;
-          }
-        }
-        h3 {
-          font-size: 1.5em;
-          ${mediaQuery.minTablet} {
-            font-size: 1.7em;
-          }
-        }
-      }
+				.description {
+					color: ${({ theme }) => theme.colors.font1};
+					font-weight: 500;
+					font-size: 2.7rem;
+					line-height: 3.5rem;
+					font-size: 2em;
+					font-family: 'Intro Demo', sans-serif;
 
-      .list-about {
-        margin: 1rem 1.5rem;
+					${mediaQuery.minTablet} {
+						font-size: 5.4rem;
+						line-height: 4.5rem;
+					}
+				}
+				h3 {
+					font-size: 1.5em;
+					${mediaQuery.minTablet} {
+						font-size: 1.7em;
+					}
+				}
+			}
 
-        ul {
-          li {
-            width: auto;
-            background: transparent;
-            color: ${({ theme }) => theme.colors.font3};
-            list-style: none;
-            margin-bottom: 0.5rem;
+			.list-about {
+				margin: 1rem 1.5rem;
 
-            svg {
-              margin-right: 0.7rem;
-            }
-          }
-        }
-      }
-    }
-    .content-img {
-      margin-top: 1rem;
-      text-align: center;
-      .wrapper-form {
-        width: 100%;
-        max-width: 30em;
-        background: ${({ theme }) => theme.colors.secondary};
-        border-radius: 10px;
-        padding: 1.5rem 2rem;
-        color: ${({ theme }) => theme.colors.dark};
-      }
-    }
-  }
+				ul {
+					li {
+						width: auto;
+						background: transparent;
+						color: ${({ theme }) => theme.colors.font3};
+						list-style: none;
+						margin-bottom: 0.5rem;
+
+						svg {
+							margin-right: 0.7rem;
+						}
+					}
+				}
+			}
+		}
+		.content-img {
+			margin-top: 1rem;
+			text-align: center;
+			.wrapper-form {
+				width: 100%;
+				max-width: 30em;
+				background: ${({ theme }) => theme.colors.secondary};
+				border-radius: 10px;
+				padding: 1.5rem 2rem;
+				color: ${({ theme }) => theme.colors.dark};
+			}
+		}
+	}
 `;
--- src/components/public/layout/BaseLayout.js
+++ src/components/public/layout/BaseLayout.js
@@ -1,233 +1,235 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import { LogoExtintores } from "../../../images";
-import { useDevice } from "../../../hooks";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { Drawer } from "./Drawer";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router";
-import { Footer } from "./Footer";
-import { ButtonsFloating, WrapperComponent } from "../ui";
-import { mediaQuery } from "../../../styles/constants/mediaQuery";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { LogoExtintores } from '../../../images';
+import { useDevice } from '../../../hooks';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { Drawer } from './Drawer';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router';
+import { Footer } from './Footer';
+import { ButtonsFloating, WrapperComponent } from '../ui';
+import { mediaQuery } from '../../../styles/constants/mediaQuery';
 
-export const BaseLayout = ({ children }) => {
-  const { isMobile } = useDevice();
-  const navigate = useNavigate();
+export const BaseLayout = ({ children, onClickVisibleFormContact }) => {
+	const { isMobile } = useDevice();
+	const navigate = useNavigate();
 
-  const [visibleDrawer, setVisibleDrawer] = useState(false);
+	const [visibleDrawer, setVisibleDrawer] = useState(false);
 
-  return (
-    <Container>
-      <Drawer
-        visibleDrawer={visibleDrawer}
-        onSetVisibleDrawer={setVisibleDrawer}
-      />
-      <header className="header">
-        <WrapperComponent>
-          <>
-            {isMobile ? (
-              <div className="menu-mobile">
-                <div className="item-logo">
-                  <img
-                    src={LogoExtintores}
-                    alt="Logo Servitec Facil Factura"
-                    onClick={() => navigate("/")}
-                  />
-                </div>
-                <div
-                  className="icon-bar"
-                  onClick={() => setVisibleDrawer(true)}
-                >
-                  <FontAwesomeIcon icon={faBars} size="2x" />
-                </div>
-              </div>
-            ) : (
-              <div className="menu-list">
-                <div className="logo-img">
-                  <Link to="/">
-                    <img
-                      src={LogoExtintores}
-                      alt="Logo Servitec Facil Factura"
-                    />
-                  </Link>
-                </div>
-                <nav className="list">
-                  <ul>
-                    <a href="#inicio">
-                      <li>INICIO</li>
-                    </a>
-                    <a href="#about-us">
-                      <li>NOSOTROS</li>
-                    </a>
-                    <a href="#services">
-                      <li>SERVICIOS</li>
-                    </a>
-                    <a href="#footer">
-                      <li>CONTÁCTO</li>
-                    </a>
-                  </ul>
-                </nav>
-              </div>
-            )}
-          </>
-        </WrapperComponent>
-      </header>
-      <main className="body">{children}</main>
-      <Footer />
-      <ButtonsFloating />
-    </Container>
-  );
+	return (
+		<Container>
+			<Drawer visibleDrawer={visibleDrawer} onSetVisibleDrawer={setVisibleDrawer} />
+			<header className="header">
+				<WrapperComponent>
+					<>
+						{isMobile ? (
+							<div className="menu-mobile">
+								<div className="item-logo">
+									<img
+										src={LogoExtintores}
+										alt="Logo Servitec Facil Factura"
+										onClick={() => navigate('/')}
+									/>
+								</div>
+								<div
+									className="icon-bar"
+									onClick={() => setVisibleDrawer(true)}
+								>
+									<FontAwesomeIcon icon={faBars} size="2x" />
+								</div>
+							</div>
+						) : (
+							<div className="menu-list">
+								<div className="logo-img">
+									<Link to="/">
+										<img
+											src={LogoExtintores}
+											alt="Logo Servitec Facil Factura"
+										/>
+									</Link>
+								</div>
+								<nav className="list">
+									<ul>
+										<a href="#inicio">
+											<li>INICIO</li>
+										</a>
+										<a href="#about-us">
+											<li>NOSOTROS</li>
+										</a>
+										<a href="#services">
+											<li>SERVICIOS</li>
+										</a>
+										<span
+											onClick={() =>
+												onClickVisibleFormContact(true)
+											}
+										>
+											<li>CONTÁCTO</li>
+										</span>
+									</ul>
+								</nav>
+							</div>
+						)}
+					</>
+				</WrapperComponent>
+			</header>
+			<main className="body">{children}</main>
+			<Footer />
+			<ButtonsFloating />
+		</Container>
+	);
 };
 
 const Container = styled.div`
-  width: 100vw;
-  min-height: 100vh;
-  height: auto;
-  position: relative;
-  padding-top: 5rem;
+	width: 100vw;
+	min-height: 100vh;
+	height: auto;
+	position: relative;
+	padding-top: 5rem;
 
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    color: ${({ theme }) => theme.colors.font2};
-    font-family: "Intro Demo", sans-serif;
-  }
+	h1,
+	h2,
+	h3,
+	h4,
+	h5,
+	h6 {
+		color: ${({ theme }) => theme.colors.font2};
+		font-family: 'Intro Demo', sans-serif;
+	}
 
-  h1 {
-    font-size: 2.7rem;
-    font-weight: bold;
-    color: ${({ theme }) => theme.colors.font1};
-    ${mediaQuery.minTablet} {
-      font-size: 6rem;
-    }
-  }
+	h1 {
+		font-size: 2.7rem;
+		font-weight: bold;
+		color: ${({ theme }) => theme.colors.font1};
+		${mediaQuery.minTablet} {
+			font-size: 6rem;
+		}
+	}
 
-  h2 {
-    font-size: 2.1rem;
-    font-weight: bold;
-    color: ${({ theme }) => theme.colors.font1};
+	h2 {
+		font-size: 2.1rem;
+		font-weight: bold;
+		color: ${({ theme }) => theme.colors.font1};
 
-    ${mediaQuery.minTablet} {
-      font-size: 5rem;
-    }
-  }
+		${mediaQuery.minTablet} {
+			font-size: 5rem;
+		}
+	}
 
-  h3 {
-    font-size: 3rem;
-    font-weight: bold;
-  }
+	h3 {
+		font-size: 3rem;
+		font-weight: bold;
+	}
 
-  h4 {
-    font-size: 1.2rem;
-    font-weight: bold;
-  }
+	h4 {
+		font-size: 1.2rem;
+		font-weight: bold;
+	}
 
-  h5 {
-    font-size: 1.1rem;
-    font-weight: bold;
-  }
+	h5 {
+		font-size: 1.1rem;
+		font-weight: bold;
+	}
 
-  p,
-  ul,
-  li,
-  span {
-    color: ${({ theme }) => theme.colors.font3};
-  }
+	p,
+	ul,
+	li,
+	span {
+		color: ${({ theme }) => theme.colors.font3};
+	}
 
-  .header {
-    position: fixed;
-    z-index: 100;
-    width: 100%;
-    //max-width: 1250px;
-    height: auto;
-    background: ${({ theme }) => theme.colors.basic};
-    margin: auto;
-    padding: 1rem;
-    display: grid;
-    grid-area: auto;
-    box-shadow: 0 6px 8px 0 rgb(12 0 46 / 6%);
-    right: 0;
-    left: 0;
-    top: 0;
+	.header {
+		position: fixed;
+		z-index: 100;
+		width: 100%;
+		//max-width: 1250px;
+		height: auto;
+		background: ${({ theme }) => theme.colors.basic};
+		margin: auto;
+		padding: 1rem;
+		display: grid;
+		grid-area: auto;
+		box-shadow: 0 6px 8px 0 rgb(12 0 46 / 6%);
+		right: 0;
+		left: 0;
+		top: 0;
 
-    .menu-list {
-      display: flex;
-      padding: 0.2em 2em;
+		.menu-list {
+			display: flex;
+			padding: 0.1em 2em;
 
-      .logo-img {
-        width: 40%;
-        list-style: none;
-        display: flex;
-        align-items: center;
-        justify-content: flex-start;
-        img {
-          width: 14rem;
-        }
-      }
+			.logo-img {
+				width: 100%;
+				list-style: none;
+				display: flex;
+				align-items: center;
+				justify-content: flex-start;
+				img {
+					width: 10em;
+				}
+			}
 
-      .list {
-        width: 60%;
-        display: flex;
-        align-items: center;
-        justify-content: flex-end;
-        ul {
-          list-style: none;
-          display: flex;
-          justify-content: space-around;
-          align-items: center;
-          padding: 0;
-          margin: 0;
-          li {
-            color: rgb(11, 12, 16);
-            font-weight: bold;
-            font-size: 1.1rem;
-            font-family: inherit !important;
-            border-bottom: 2px solid transparent;
-            transition: all 0.3s ease-in-out;
-            margin-left: 1.5rem;
-            cursor: pointer;
-          }
-          li:hover {
-            color: rgb(121, 131, 140);
-            border-bottom: 2px solid rgb(246, 70, 93);
-            transition: all 0.3s ease-in-out;
-          }
-        }
-      }
-    }
+			.list {
+				width: 60%;
+				display: flex;
+				align-items: center;
+				justify-content: flex-end;
+				ul {
+					list-style: none;
+					display: flex;
+					justify-content: space-around;
+					align-items: center;
+					padding: 0;
+					margin: 0;
+					li {
+						color: rgb(11, 12, 16);
+						font-weight: bold;
+						font-size: 1.1rem;
+						font-family: inherit !important;
+						border-bottom: 2px solid transparent;
+						transition: all 0.3s ease-in-out;
+						margin-left: 1.5rem;
+						cursor: pointer;
+					}
+					li:hover {
+						color: rgb(121, 131, 140);
+						border-bottom: 2px solid rgb(246, 70, 93);
+						transition: all 0.3s ease-in-out;
+					}
+				}
+			}
+		}
 
-    .menu-mobile {
-      width: 100%;
-      color: ${({ theme }) => theme.colors.black};
-      display: grid;
-      grid-template-columns: 85% 1fr;
-      .item-logo {
-        display: flex;
-        align-items: center;
-        justify-content: start;
-        img {
-          width: 65%;
-          max-width: 220px;
-        }
-      }
+		.menu-mobile {
+			width: 100%;
+			color: ${({ theme }) => theme.colors.black};
+			display: grid;
+			grid-template-columns: auto 1fr;
+			justify-items: end;
+			.item-logo {
+				display: flex;
+				align-items: center;
+				justify-content: start;
+				img {
+					width: 100%;
+					max-width: 150px;
+				}
+			}
 
-      .icon-bar {
-        width: auto;
-        height: 100%;
-        padding: 0.7rem 0.1rem 0.7rem 1.7rem;
-      }
-    }
-  }
+			.icon-bar {
+				width: auto;
+				height: 100%;
+				padding: 0.7rem 0.1rem 0.7rem 1.7rem;
+			}
+		}
+	}
 
-  .footer {
-    width: 100%;
-    height: auto;
-    padding: 1rem;
-    background: black;
-    color: #fff;
-  }
+	.footer {
+		width: 100%;
+		height: auto;
+		padding: 1rem;
+		background: black;
+		color: #fff;
+	}
 `;
